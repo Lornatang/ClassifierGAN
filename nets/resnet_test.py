d8f2adf47720fc96a78bc5acbcfe2946bceb4398
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-22 14:14:04 +0800

    Create resnet_test.py

---
+++ nets/resnet_test.py
@@ -0,0 +1,202 @@
+# Copyright 2019 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import torch
+import torch.nn as nn
+
+__all__ = ['ResNet', 'resnet18', 'resnet34',
+           'resnet50', 'resnet101', 'resnet152']
+
+
+def conv3x3(in_planes, out_planes, stride=1, dilation=1):
+  """3x3 convolution with padding"""
+  return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                   padding=dilation, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+  """1x1 convolution"""
+  return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+  expansion = 1
+
+  def __init__(self, inplanes, planes, stride=1, downsample=None):
+    super(BasicBlock, self).__init__()
+    # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+    self.conv1 = conv3x3(inplanes, planes, stride)
+    self.bn1 = nn.BatchNorm2d(planes)
+    self.relu = nn.ReLU(inplace=True)
+    self.conv2 = conv3x3(planes, planes)
+    self.bn2 = nn.BatchNorm2d(planes)
+    self.downsample = downsample
+    self.stride = stride
+
+  def forward(self, x):
+    identity = x
+
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
+
+    out = self.conv2(out)
+    out = self.bn2(out)
+
+    if self.downsample is not None:
+      identity = self.downsample(x)
+
+    out += identity
+    out = self.relu(out)
+
+    return out
+
+
+class Bottleneck(nn.Module):
+  expansion = 4
+
+  def __init__(self, inplanes, planes, stride=1, downsample=None):
+    super(Bottleneck, self).__init__()
+    # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(planes)
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                           padding=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(planes)
+    self.conv3 = nn.Conv2d(inplanes, planes * 4, kernel_size=1, bias=False)
+    self.bn3 = nn.BatchNorm2d(planes * 4)
+    self.relu = nn.ReLU(inplace=True)
+    self.downsample = downsample
+    self.stride = stride
+
+  def forward(self, x):
+    identity = x
+
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
+
+    out = self.conv2(out)
+    out = self.bn2(out)
+    out = self.relu(out)
+
+    out = self.conv3(out)
+    out = self.bn3(out)
+
+    if self.downsample is not None:
+      identity = self.downsample(x)
+
+    out += identity
+    out = self.relu(out)
+
+    return out
+
+
+class ResNet(nn.Module):
+
+  def __init__(self, block, layers, num_classes=10):
+    self.inplanes = 64
+    super(ResNet, self).__init__()
+    self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1,
+                           bias=False)
+    self.bn1 = nn.BatchNorm2d(64)
+    self.relu = nn.ReLU(inplace=True)
+    self.layer1 = self._make_layer(block, 64, layers[0])
+    self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+    self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+    self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+    self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+    for m in self.modules():
+      if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+      elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+
+  def _make_layer(self, block, planes, blocks, stride=1):
+    downsample = None
+    if stride != 1 or self.inplanes != planes * block.expansion:
+      downsample = nn.Sequential(
+        nn.Conv2d(self.inplanes, planes * block.expansion,
+                  kernel_size=1, stride=stride, bias=False),
+        nn.BatchNorm2d(planes * block.expansion),
+      )
+
+    layers = [block(self.inplanes, planes, stride, downsample)]
+    self.inplanes = planes * block.expansion
+    for _ in range(1, blocks):
+      layers.append(block(self.inplanes, planes))
+
+    return nn.Sequential(*layers)
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.bn1(x)
+    x = self.relu(x)
+
+    x = self.layer1(x)
+    x = self.layer2(x)
+    x = self.layer3(x)
+
+    x = self.avgpool(x)
+    x = torch.flatten(x, 1)
+    x = self.fc(x)
+
+    return x
+
+
+def _resnet(block, layers, **kwargs):
+  model = ResNet(block, layers, **kwargs)
+  return model
+
+
+def resnet18(**kwargs):
+  r"""ResNet-18 model from
+  `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+  """
+  return _resnet(BasicBlock, [2, 2, 2, 2], **kwargs)
+
+
+def resnet34(**kwargs):
+  r"""ResNet-34 model from
+  `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+  """
+  return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], **kwargs)
+
+
+def resnet50(**kwargs):
+  r"""ResNet-50 model from
+  `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+  """
+  return _resnet('resnet50', BasicBlock, [3, 4, 6, 3], **kwargs)
+
+
+def resnet101(**kwargs):
+  r"""ResNet-101 model from
+  `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+  """
+  return _resnet('resnet101', BasicBlock, [3, 4, 23, 3], **kwargs)
+
+
+def resnet152(**kwargs):
+  r"""ResNet-152 model from
+  `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+  """
+  return _resnet('resnet152', BasicBlock, [3, 8, 36, 3], **kwargs)
