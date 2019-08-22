47e4ad37a679ad10babc523d758d9a5d21ac712e
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-22 14:15:04 +0800

    Update resnet.py

--- nets/resnet.py
+++ nets/resnet.py
@@ -194,9 +194,8 @@ class ResNet(nn.Module):
         norm_layer(planes * block.expansion),
       )
 
-    layers = []
-    layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                        self.base_width, previous_dilation, norm_layer))
+    layers = [block(self.inplanes, planes, stride, downsample, self.groups,
+                    self.base_width, previous_dilation, norm_layer)]
     self.inplanes = planes * block.expansion
     for _ in range(1, blocks):
       layers.append(block(self.inplanes, planes, groups=self.groups,
