3cb5954305ce6d816363bacba5953a71d0de0ed1
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-22 14:35:19 +0800

    Update adjust.py

--- utils/adjust.py
+++ utils/adjust.py
@@ -15,8 +15,8 @@
 """Dynamic adjustment of parameters tool implementation"""
 
 
-def adjust_learning_rate(initial_lr, optimizer, epoch):
+def adjust_learning_rate(initial_lr, optimizer, epoch, every_epoch):
   """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-  lr = initial_lr * (0.1 ** (epoch // 30))
+  lr = initial_lr * (0.1 ** (epoch // every_epoch))
   for param_group in optimizer.param_groups:
     param_group['lr'] = lr
