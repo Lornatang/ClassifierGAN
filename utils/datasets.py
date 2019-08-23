e2059d37686fe687c7023088337af92b394599ca
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-23 23:21:30 +0800

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -73,6 +73,33 @@ def load_datasets(name, root, batch_size):
                                                   shuffle=False, num_workers=8)
     return train_dataloader, test_dataloader
 
+  elif name == "qmnist":
+    train_dataset = datasets.QMNIST(root=root,
+                                    download=True,
+                                    train=True,
+                                    transform=transforms.Compose([
+                                      transforms.Resize(28),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.5], [0.5]),
+                                    ]))
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                                   shuffle=True, num_workers=8)
+    test_dataset = datasets.QMNIST(root=root,
+                                   download=True,
+                                   what="test50k",
+                                   train=False,
+                                   transform=transforms.Compose([
+                                     transforms.Resize(28),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.5], [0.5]),
+                                   ]))
+
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                                  shuffle=False, num_workers=8)
+    return train_dataloader, test_dataloader
+
   elif name == "cifar10":
     train_dataset = datasets.CIFAR10(root=root,
                                      download=True,
